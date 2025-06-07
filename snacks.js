a10aa710405fbd58caba3a8e3efd5d50289cd7cc
NavodContrath <navod.contrath@gmail.com>
2025-06-07 12:12:08 +0200

    first question

--- snacks.js
+++ snacks.js
@@ -5,10 +5,11 @@ const secondBurger = hamburger;
 secondBurger.name = 'Double Cheese Burger';
 secondBurger.weight = 500;
 ​
-console.log(hamburger.name); // ?
-console.log(secondBurger.name ); // ?
-Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
-Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+console.log(hamburger.name); // name: 'Double Cheese Burger'
+console.log(secondBurger.name ); // name: 'Double Cheese Burger'
+
+Senza lanciare il codice, riesci a prevedere cosa viene stampato in console? si.
+Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 1
 🏆 Code Question 2
 
 console.log(ham
